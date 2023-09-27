08d329fecb44594c47f531761e78e8f205901283
awildcatfox <130520774+awildcatfox@users.noreply.github.com>
2023-09-26 17:51:51 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Github_Practice_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How was your day will ?");
+        }
+    }
+}
